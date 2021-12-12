dfa2530a1b3533e1cc6a300da9d23e81019a1c6e
Willam Esnehider Mahecha <wsmh99@hotmail.com>
2021-12-11 21:15:54 -0500

    Ajuste

--- src/main/java/com/mahecha/william/reto2ciclo4a/Reto2Ciclo4aApplication.java
+++ src/main/java/com/mahecha/william/reto2ciclo4a/Reto2Ciclo4aApplication.java
@@ -23,8 +23,8 @@ public class Reto2Ciclo4aApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         userCrudRepository.deleteAll();
-        userCrudRepository.saveAll(List.of(
+        /*userCrudRepository.saveAll(List.of(
                 new User(1, "1118557622", "William Mahecha", "cll 38 # 44 - 51", "3208088701", "wsmh99@hotmail.com", "Demo", "Zona1", "ADM")
-        ));
+        ));*/
     }
 }
